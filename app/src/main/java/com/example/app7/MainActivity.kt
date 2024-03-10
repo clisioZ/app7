04cc68ec2ef05385099a783fe3dbc5044894e50e
clisioZ <juan.zepeda7977@alumnos.udg.mx>
2024-03-10 14:24:39 -0600

    Version 1

---
+++ app/src/main/java/com/example/app7/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.app7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//import com.airbnb.lottie.LottieAnimationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //val lottieAnimationView = findViewById<LottieAnimationView>(R.id.lottieAnimationView)
+        //lottieAnimationView.setAnimation("bus_animation.json")
+        //lottieAnimationView.playAnimation()
+    }
+}
